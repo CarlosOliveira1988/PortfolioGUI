b2fc76d2c6b89157d78ae27200c5bc4c12c00c69
CarlosOliveira1988 <CarlosOliveira1988@users.noreply.github.com>
2022-07-09 14:08:55 -0300

    Solving the issue to save the Extrato file path
    - Remove the 'easygui' usage
    - Replacing the 'easygui' for an workaround based on Streamlit components

--- Extrato.py
+++ Extrato.py
@@ -1,4 +1,5 @@
-from easygui import fileopenbox
+import os
+
 import streamlit as st
 
 from extrato_lib.extrato_file_manager import FileManager
@@ -20,33 +21,50 @@ class ExtratoGUI:
     def __showMainTitle(self) -> None:
         st.write('# Extrato')
 
-    def __showFileSelector(self) -> None:
-        clicked = st.button("Carregar planilha")
-        if clicked:
-            self.__openFile()
+    def __showFolderSelector(self) -> None:
+        folder_path_default = os.path.dirname(st.session_state.extrato_file)
+        self.folder_path = st.text_input(
+            "Digite no campo o local do arquivo Extrato: ",
+            value = folder_path_default,
+        )
+
+    def __showFileSelector(self):
+        filenames = [file for file in os.listdir(self.folder_path) if file.endswith((".xls", ".xlsx"))]
+        selected_filename = st.selectbox('Selecione o arquivo Extrato: ', filenames)
+        self.file_path = os.path.join(self.folder_path, selected_filename)
+
+    def __showSelectedFile(self):
         st.write('Planilha atual: ', st.session_state.extrato_file)
+
+    def __showUpdateFileButton(self):
+        if st.button("Selecionar"):
+            self.__updateExtratoFile()        
+
+    def __updateExtratoFile(self):
+        if os.path.isfile(self.file_path):
+            if self.file_path != st.session_state.extrato_file:
+                self.file_manager.setExtratoFile(self.file_path)
+                st.session_state.extrato_file = self.file_manager.getExtratoFile()
+                self.__showBadBehaviorInfo()
+
+    def __showBadBehaviorInfo(self):
+        st.write('Planilha selecionada: ', st.session_state.extrato_file)
+        st.markdown(
+            """
+            :disappointed_relieved: Por limitações do Streamlit, ainda não resolvidas...
+            
+            :sweat_smile: **Por favor, reinicie a aplicação para carregar o novo arquivo!**
+            É necessário fechar tanto a página quanto o prompt de comandos.
+            """
+        )
+        st.stop()
 
     def __showInfo(self) -> None:
         self.__showMainTitle()
+        self.__showFolderSelector()
         self.__showFileSelector()
-
-    def __openFile(self):
-        file_path = fileopenbox(
-            "Selecione o arquivo XLSX relacionado ao portfolio",
-            filetypes = ["*.xlsx", "*.xls"]
-        )
-        if file_path:
-            self.file_manager.setExtratoFile(file_path)
-            st.session_state.extrato_file = self.file_manager.getExtratoFile()
-            st.markdown(
-                """
-                :disappointed_relieved: Por limitações do Streamlit, ainda não resolvidas...
-                
-                :sweat_smile: **Por favor, reinicie a aplicação para carregar o novo arquivo!**
-                É necessário fechar tanto a página quanto o prompt de comandos.
-                """
-            )
-            st.stop()
+        self.__showSelectedFile()
+        self.__showUpdateFileButton()
 
 
 extrato_gui = ExtratoGUI()
