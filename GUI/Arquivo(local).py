da72525e2e7f15042d0f52728ef0adfd127d94f4
CarlosOliveira1988 <CarlosOliveira1988@users.noreply.github.com>
2022-08-07 20:04:02 -0300

    Changing the page names
    Changing the page names, in order to improve the user experience

--- GUI/Arquivo(local).py
+++ GUI/Arquivo(local).py
@@ -4,7 +4,7 @@ import streamlit as st
 
 from extrato.lib.extrato_file_manager import FileManager
 
-from GUI.Extrato import SessionStateControl
+from GUI.Arquivo import SessionStateControl
 
 
 class ExtratoGuiLocal:
--- main.bat
+++ main.bat
@@ -1,1 +1,1 @@
-python -m streamlit run GUI/Extrato_local.py
+python -m streamlit run GUI/Arquivo(local).py
